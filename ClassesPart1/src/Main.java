e3a694f3b65950d757f09b13ebad8fc292ecb284
John Stading <82844073+jstading@users.noreply.github.com>
2024-10-14 13:08:23 -0500

    Add files via upload

--- ClassesPart1/src/Main.java
+++ ClassesPart1/src/Main.java
@@ -1,12 +1,37 @@
 public class Main {
 
     public static void main(String[] args){
+
         Car car = new Car();
+        car.setMake("Porsche");
+        car.setModel("Carrera");
+        car.setColor("Red");
+        car.setDoors(2);
+        car.setConvertible(true);
+
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
         System.out.println("color = " + car.getColor());
         System.out.println("doors = " + car.getDoors());
-        System.out.println("convertible = " + car.getConvertible());
+        System.out.println("convertible = " + car.isConvertible());
         car.describeCar();
+        System.out.println();
+
+        Car targa = new Car();
+
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setColor("Red");
+        targa.setDoors(2);
+        targa.setConvertible(false);
+
+        System.out.println("make = " + targa.getMake());
+        System.out.println("model = " + targa.getModel());
+        System.out.println("color = " + targa.getColor());
+        System.out.println("doors = " + targa.getDoors());
+        System.out.println("convertible = " + targa.isConvertible());
+        targa.describeCar();
+
+
     }
 }
